0775259bda12320fe68562c8f1538a94e3496bb4
木比白 <rrkai@qq.com>
2021-03-09 21:43:47 +0800

    update app/src/main/java/com/vone/vmq/NeNotificationService2.java.

--- app/src/main/java/com/vone/vmq/NeNotificationService2.java
+++ app/src/main/java/com/vone/vmq/NeNotificationService2.java
@@ -142,27 +142,8 @@ public class NeNotificationService2  extends NotificationListenerService {
 
                 if (pkg.equals("com.eg.android.AlipayGphone")){
                     if (content!=null && !content.equals("")) {
-                        if (content.indexOf("通过扫码向你付款")!=-1 || content.indexOf("成功收款")!=-1){
+                        if (content.indexOf("通过扫码向你付款")!=-1 || title.indexOf("成功收款")!=-1 || content.indexOf("成功收款")!=-1){
                             String money = getMoney(content);
-                            if (money!=null){
-                                Log.d(TAG, "onAccessibilityEvent: 匹配成功： 支付宝 到账 " + money);
-                                appPush(2, Double.valueOf(money));
-                            }else {
-                                Handler handlerThree=new Handler(Looper.getMainLooper());
-                                handlerThree.post(new Runnable(){
-                                    public void run(){
-                                        Toast.makeText(getApplicationContext() ,"监听到支付宝消息但未匹配到金额！",Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-                            }
-                        }
-
-                    }
-
-                if (pkg.equals("com.eg.android.AlipayGphone")){
-                    if (content!=null && !content.equals("")) {
-                        if (title.indexOf("通过扫码向你付款")!=-1 || title.indexOf("成功收款")!=-1){
-                            String money = getMoney(title);
                             if (money!=null){
                                 Log.d(TAG, "onAccessibilityEvent: 匹配成功： 支付宝 到账 " + money);
                                 appPush(2, Double.valueOf(money));
